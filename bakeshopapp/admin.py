02b19fa12bb422fe91242ac9320399eeaa4e449f
Rostislav <rostwik@gmail.com>
2023-07-26 17:16:39 +0300

    Add admin

--- bakeshopapp/admin.py
+++ bakeshopapp/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
 
+from bakeshopapp.models import Order, Level, Shape, Topping, Berry, Decor
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'firstname', 'lastname', 'phonenumber', 'address', 'restaurant')
+    pass
+
+
+@admin.register(Level)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Shape)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topping)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Berry)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Decor)
+class OrderAdmin(admin.ModelAdmin):
+    pass
