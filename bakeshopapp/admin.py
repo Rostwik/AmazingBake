50a83fb4149d6c67bac61e454d7d03617913c9c8
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-07-27 10:29:59 +0300

    Add export to csv with actions (admin/OrderAdmin)

--- bakeshopapp/admin.py
+++ bakeshopapp/admin.py
@@ -1,38 +1,81 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from bakeshopapp.models import Order, Level, Shape, Topping, Berry, Decor, Cake
+from .models import Order, Level, Shape, Topping, Berry, Decor, Cake, Customer
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """Возвращает файл с данными в формате CSV"""
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%Y-%m-%d')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'id', 'cake', 'status', 'customer', 'comment',
+        'delivery_address', 'delivery_date', 'delivery_time',
+        'total', # order_detail
+    ]
+    list_display_links = [
+        'id', 'cake',
+    ]
+    actions = [export_to_csv]
 
 
 @admin.register(Level)
-class OrderAdmin(admin.ModelAdmin):
+class LevelAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Shape)
-class OrderAdmin(admin.ModelAdmin):
+class ShapeAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Topping)
-class OrderAdmin(admin.ModelAdmin):
+class ToppingAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Berry)
-class OrderAdmin(admin.ModelAdmin):
+class BerryAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Decor)
-class OrderAdmin(admin.ModelAdmin):
+class DecorAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Cake)
 class CakeAdmin(admin.ModelAdmin):
     list_filter = ['kind']
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
