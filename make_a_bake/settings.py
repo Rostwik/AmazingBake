b4295aa766f9c11ed7e7f521c0e085ca72efdf20
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-07-27 16:09:20 +0300

    Register app marketing

--- make_a_bake/settings.py
+++ make_a_bake/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 'bakeshopapp.apps.BakeshopappConfig',
     'bakeshopapp',
+    'marketing',
 
 ]
 
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BITLY_TOKEN = env.str('BITLY_TOKEN')
